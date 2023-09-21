edb91ec946f47ab461d1ddc06aa3f4e8784f21b5
muhammadrohan3 <amjadt414@gmail.com>
2023-09-21 18:58:02 +0500

    book flatlist

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Login from "./LoginPage";
-import Welcome from "./Welcome";
+import Login from "./components/LoginPage";
+import Welcome from "./components/Welcome";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SafeArea from "./SafeArea";
-
+import SafeArea from "./components/SafeArea";
+import Chats from "./components/Chats";
+import Books from "./components/Books";
 const stack = createStackNavigator();
 export default function App() {
 
@@ -16,14 +17,15 @@ export default function App() {
       <SafeArea/>
       <NavigationContainer>
         <stack.Navigator
-          initialRouteName="Login"
+          initialRouteName="Books"
           screenOptions={{
             headerShown: false,
           }}
         >
-
           <stack.Screen name="Login" component={Login} />
           <stack.Screen name="Welcome" component={Welcome} />
+          <stack.Screen name="Chats" component={Chats} />
+          <stack.Screen name="Books" component={Books} />
         </stack.Navigator>  
       </NavigationContainer>
     </SafeAreaProvider>
