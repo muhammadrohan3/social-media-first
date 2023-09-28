f402714c0099b4c37b3f440708cb58a3ac4bcf3a
muhammadrohan3 <amjadt414@gmail.com>
2023-09-28 13:15:46 +0500

    api custom hook

--- components/Books.js
+++ components/Books.js
@@ -1,51 +1,52 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  FlatList,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { View, Text } from "react-native";
 import Search from "./Search";
 import BooksList from "./BooksList";
+import apiHook from "../hooks/apiHook";
+const bookData = [
+  {
+    name: "The Alchemist",
+    author: "Paulo Coelho",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/51ZU%2BCvkTyL._SX331_BO1,204,203,200_.jpg",
+  },
+  {
+    name: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/91VokXkn8hL.jpg",
+  },
+  {
+    name: "Psychology of Money",
+    author: "Morgan Housel",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL.jpg",
+  },
+  {
+    name: "Zero to One",
+    author: "Peter Thiel",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/71uAI28kJuL.jpg",
+  },
+  {
+    name: "The Lean Startup",
+    author: "Eric Ries",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg",
+  },
+  {
+    name: "Art of War",
+    author: "Sun Tzu",
+    pic: "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL.jpg",
+  },
+];
 
 function Books() {
-  const bookData = [
-    {
-      name: "The Alchemist",
-      author: "Paulo Coelho",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/51ZU%2BCvkTyL._SX331_BO1,204,203,200_.jpg",
-    },
-    {
-      name: "Rich Dad Poor Dad",
-      author: "Robert Kiyosaki",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/91VokXkn8hL.jpg",
-    },
-    {
-      name: "Psychology of Money",
-      author: "Morgan Housel",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL.jpg",
-    },
-    {
-      name: "Zero to One",
-      author: "Peter Thiel",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/71uAI28kJuL.jpg",
-    },
-    {
-      name: "The Lean Startup",
-      author: "Eric Ries",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg",
-    },
-    {
-      name: "Art of War",
-      author: "Sun Tzu",
-      pic: "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL.jpg",
-    },
-  ];
+  const [bookData, setBookData] = useState();
   const [books, setBooks] = useState(bookData);
+  const locadData = async ()=>{
+    const data = await apiHook("http://localhost:8888/api/getBooks");
+    console.log("This is new data", data);
+    setBookData(data);
+    setBooks(data);
+  }
   useEffect(() => {
-    setBooks(bookData);
+    locadData();
   }, []);
 
   const Header = () => {
@@ -54,7 +55,7 @@ function Books() {
         style={{
           width: "100%",
           height: 100,
-         
+
           justifyContent: "center",
           alignItems: "center",
         }}
---
+++ hooks/apiHook.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { View, Text } from "react-native"
+import {useEffect, useState} from 'react'
+
+
+async function apiHook(link) {
+    console.log("This is link",link)
+    
+    const data = await axios.get(link)
+    .then(res=>{
+        console.log("This is res",res)
+        return res.data.books
+    })
+    .catch(err=>{
+        console.log("This is err",err)
+    })
+    console.log("This is data", data)
+    return data
+}
+
+export default apiHook
